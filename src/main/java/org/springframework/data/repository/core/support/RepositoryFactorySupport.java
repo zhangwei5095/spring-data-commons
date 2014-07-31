e7dcf813f2d8047af3e7a59ad38e35973b9c9fbb
Oliver Gierke <ogierke@gopivotal.com>
2014-07-31 20:10:52 +0200

    DATACMNS-293 - Setting QueryAugementationEngine on queries.

--- src/main/java/org/springframework/data/repository/core/support/RepositoryFactorySupport.java
+++ src/main/java/org/springframework/data/repository/core/support/RepositoryFactorySupport.java
@@ -87,8 +87,8 @@ public abstract class RepositoryFactorySupport implements BeanClassLoaderAware {
 	private ClassLoader classLoader = org.springframework.util.ClassUtils.getDefaultClassLoader();
 	private EvaluationContextProvider evaluationContextProvider = DefaultEvaluationContextProvider.INSTANCE;
 
-	private QueryCollectingQueryCreationListener collectingListener = new QueryCollectingQueryCreationListener();
 	private QueryAugmentationEngine augmentationEngine = QueryAugmentationEngine.NONE;
+	private QueryCollectingQueryCreationListener collectingListener = new QueryCollectingQueryCreationListener();
 
 	public RepositoryFactorySupport() {
 		this.queryPostProcessors.add(collectingListener);
@@ -384,6 +384,11 @@ public abstract class RepositoryFactorySupport implements BeanClassLoaderAware {
 
 			for (Method method : queryMethods) {
 				RepositoryQuery query = lookupStrategy.resolveQuery(method, repositoryInformation, namedQueries);
+
+				if (query instanceof QueryAugmentationEngineAware) {
+					((QueryAugmentationEngineAware) query).setQueryAugmentationEngine(augmentationEngine);
+				}
+
 				invokeListeners(query);
 				queries.put(method, query);
 			}
